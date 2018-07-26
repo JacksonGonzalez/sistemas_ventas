b793187de951ac134fdc25a9c31648f8eb3ce98d
JacksonGonzalez <jaesgoca@gmail.com>
2018-07-26 18:47:20 -0500

    se crea el modelo de personas

---
+++ app/Persona.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Persona extends Model
+{
+    protected $table = 'personas';
+    protected $fillable = ['nombre', 'tipo_documento', 'num_documento', 'direccion', 'telefono', 'email'];
+    
+}

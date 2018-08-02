cdbd8756937d198dfd9674746a70854844da0dfc
JacksonGonzalez <jaesgoca@gmail.com>
2018-08-01 22:25:15 -0500

    envio parcial del controlador proveedor

---
+++ app/Http/Controllers/ProveedorController.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Proveedor;
+
+class ProveedorController extends Controller
+{
+    public function index(Request $request)
+    {
+        //validar seguridad por HTTP
+        if (!$request->ajax()) return redirect('/');
+
+        $buscar = $request->buscar;
+        $criterio = $request->criterio;
+
+        if($buscar == ''){
+            $personas = Proveedor::orderBy('id', 'DESC')->paginate(3);
+        }else{
+            $personas = Persona::where($criterio, 'like', '%'.$buscar.'%')->orderBy('id', 'desc')->paginate(3);
+        }
+
+        return [
+            'pagination' => [
+                'total'         => $personas->total(),
+                'current_page'  => $personas->currentPage(),
+                'per_page'      => $personas->perPage(),
+                'last_page'     => $personas->lastPage(),
+                'from'          => $personas->firstItem(),
+                'to'            => $personas->lastItem(),
+            ],
+            'personas' => $personas
+        ];
+    } 
+
+    public function store(Request $request)
+    {
+        //validar seguridad por HTTP
+        if(!$request->ajax()) return redirect('/');
+        $persona = new Persona();
+        $persona->nombre = $request->nombre;
+        $persona->tipo_documento = $request->tipo_documento;
+        $persona->num_documento = $request->num_documento;
+        $persona->direccion = $request->direccion;
+        $persona->telefono = $request->telefono;
+        $persona->email = $request->email;
+        $persona->save();
+    }
+
+    public function update(Request $request)
+    {
+        //validar seguridad por HTTP
+        if(!$request->ajax()) return redirect('/');
+        $persona = Persona::findOrFail($request->id);
+        $persona->nombre = $request->nombre;
+        $persona->tipo_documento = $request->tipo_documento;
+        $persona->num_documento = $request->num_documento;
+        $persona->direccion = $request->direccion;
+        $persona->telefono = $request->telefono;
+        $persona->email = $request->email;
+        $persona->save();
+    }
+}

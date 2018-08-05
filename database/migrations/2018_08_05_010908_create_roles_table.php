36de45c0d59e8ce07224760ec68d0ca80c2e12b6
JacksonGonzalez <jaesgoca@gmail.com>
2018-08-04 20:31:36 -0500

    se añade la migracion de roles

---
+++ database/migrations/2018_08_05_010908_create_roles_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateRolesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('nombre', 30)->unique();
+            $table->string('descripcion', 100)->nullable();
+            $table->boolean('condicion')->default(1);
+            
+        });
+
+        DB::table('roles')->insert(array('id' => '1', 'nombre' => 'Administrador', 'descripcion' => 'Administradores del sistema'));
+        DB::table('roles')->insert(array('id' => '2', 'nombre' => 'Vendedor', 'descripcion' => 'Rol Vendedores'));
+        DB::table('roles')->insert(array('id' => '3', 'nombre' => 'Almacenero', 'descripcion' => 'Rol del almacen'));
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('roles');
+    }
+}

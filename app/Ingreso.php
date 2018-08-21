5678d2b3e7fac40d511a7f5f590332c0c049bbe9
JacksonGonzalez <jaesgoca@gmail.com>
2018-08-20 22:13:33 -0500

    se añade los modelos de Ingreso y DetalleIngreso

---
+++ app/Ingreso.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Ingreso extends Model
+{
+    protected $fillable = [
+        'idproveedor', 'idusuario', 'tipo_comprobante', 'num_comprobante', 'fecha_hora', 'impuesto', 'total', 'estado'
+    ];
+
+    public function usuario(){
+        return $this->belongsTo('App\User');
+    }
+
+    public function proveedor(){
+        return $this->belongsTo('App\Proveedor');
+    }
+}
